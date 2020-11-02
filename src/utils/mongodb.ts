46406545a6735f2af0c67a4a3baada9bf4fb7899
Matheus Calegaro <matheuscalegaro@outlook.com>
2020-11-01 23:53:48 -0300

    feat(database): configuring mongo instance

---
+++ src/utils/mongodb.ts
@@ -0,0 +1,50 @@
+import { MongoClient } from 'mongodb';
+
+const { DATABASE_URL, DATABASE_NAME } = process.env;
+
+if (!DATABASE_URL) {
+  throw new Error('Please define the DATABASE_URL environment variable inside .env.local');
+}
+
+if (!DATABASE_NAME) {
+  throw new Error('Please define the DATABASE_NAME environment variable inside .env.local');
+}
+
+// @ts-ignore
+let cached = global.mongo;
+
+if (!cached) {
+  // @ts-ignore
+  cached = global.mongo = {};
+}
+
+export async function connectToDatabase(): Promise<any> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const conn = {};
+
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = MongoClient.connect(DATABASE_URL, opts)
+      .then(client => {
+        // @ts-ignore
+        conn.client = client;
+        return client.db(DATABASE_NAME);
+      })
+      .then(db => {
+        // @ts-ignore
+        conn.db = db;
+        cached.conn = conn;
+      });
+  }
+
+  await cached.promise;
+
+  return cached.conn;
+}
