fa803bbaa25c33cd733be6b6807bd1fd705ce30c
Matheus Calegaro <matheuscalegaro@outlook.com>
2020-11-21 19:21:37 -0300

    chore: add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 
+import '../assets/css/styles.css';
+
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <Provider session={pageProps.session}>
