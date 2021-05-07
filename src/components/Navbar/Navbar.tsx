26816638f2d42d5512b4a7c063a3662a9168be39
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-05-07 18:23:43 -0300

    refactor(components/navbar): styling with ChakraUI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,44 +1,70 @@
-import React from 'react';
-import { signOut } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+import { getSession, signOut } from 'next-auth/client';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
-export const Navbar = (): JSX.Element => (
-  <div className="relative bg-white shadow">
-    <header className="px-6 py-2 lg:px-16 lg:py-0 bg-white flex flex-wrap items-center">
-      <div className="flex-1 flex justify-between items-center">
-        <a href="#">
-          <h3 className="text-2xl font-bold">OneTime</h3>
-        </a>
-      </div>
+export const Navbar = (): JSX.Element => {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [imageUrl, setImageUrl] = useState<string>('');
 
-      <label htmlFor="menu-toggle" className="pointer-cursor lg:hidden block">
-        <svg
-          className="fill-current text-gray-900"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-        >
-          <title>menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-        </svg>
-      </label>
+  useEffect(() => {
+    async function getUserData() {
+      const session = await getSession();
 
-      <input className="hidden" type="checkbox" id="menu-toggle" />
+      setName(session.user.name);
+      setEmail(session.user.email);
+      setImageUrl(session.user.image);
+    }
 
-      <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
-        <nav>
-          <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-            <li>
-              <a
-                className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 cursor-pointer"
+    getUserData();
+  }, []);
+
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <HStack spacing={8} alignItems="center">
+          <Box>Onetime</Box>
+        </HStack>
+
+        <Flex alignItems="center">
+          <Menu>
+            <MenuButton as={Button} rounded="full" variant="link" cursor="pointer">
+              <Avatar size="sm" src={imageUrl} />
+            </MenuButton>
+
+            <MenuList>
+              <MenuItem as="div" style={{ display: 'block' }}>
+                <Text fontSize="sm">{name}</Text>
+                <Text fontSize="xs" color="gray.500">
+                  {email}
+                </Text>
+              </MenuItem>
+
+              <MenuDivider />
+
+              <MenuItem
+                fontSize="sm"
                 onClick={() => signOut({ callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })}
               >
-                Sign Out
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  </div>
-);
+                Sign out
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
