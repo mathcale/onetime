03fc398c9106805e034827657d2bc3d91ea4a368
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-05-07 18:24:48 -0300

    chore(ui): forcing dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,15 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from '../utils/theme';
+
 import 'notyf/notyf.min.css';
 import '../assets/css/styles.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <Provider session={pageProps.session}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </Provider>
